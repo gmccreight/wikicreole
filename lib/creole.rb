254d2099e2cc1eb478d52592d496db62a6e0ec0f
Gordon McCreight <gordon@mccreight.com>
2008-12-03 17:08:44 -0800

    compiled regular expressions will be faster

--- lib/creole.rb
+++ lib/creole.rb
@@ -2,16 +2,22 @@ class Creole
 
 	# Most of this code is based off of Jason Burnett's excellent Perl-based converter which can be found here:
 	# http://search.cpan.org/~jburnett/Text-WikiCreole/
+	
+	@compiled_strip_eq_1 = Regexp.compile(/^\s*=*\s*/)
+	@compiled_strip_eq_2 = Regexp.compile(/\s*=*\s*$/)
 
 	def self.strip_leading_and_trailing_eq_and_whitespace(s)
-	  s.sub!(/^\s*=*\s*/, '')
-	  s.sub!(/\s*=*\s*$/, '')
+	  s.sub!(@compiled_strip_eq_1, '')
+	  s.sub!(@compiled_strip_eq_2, '')
 	end
 
-	# strip list markup trickery
-	def self.strip_list(s)
-	  s.gsub!(/(?:`*| *)[\*\#]/, '`')
-	  s.gsub!(/\n(?:`*| *)[\*\#]/, "\n`")
-	end
+	# @compiled_list_strip_1 = Regexp.compile(/(?:`*| *)[\*\#]/)
+	# @compiled_list_strip_2 = Regexp.compile(/\n(?:`*| *)[\*\#]/)
+	
+	# # strip list markup trickery
+	# def self.strip_list(s)
+	  # s.gsub!(@compiled_list_strip_1, '`')
+	  # s.gsub!(@compiled_list_strip_2, "\n`")
+	# end
 
 end
--- tests/runner.rb
+++ tests/runner.rb
@@ -11,4 +11,8 @@ class TC_Creole < Test::Unit::TestCase
 	assert_equal "head", Creole.strip_leading_and_trailing_eq_and_whitespace("head ===")
   end
   
+  def test_strip_list
+    #assert_equal "`head", Creole.strip_leading_and_trailing_eq_and_whitespace(" **head")
+  end
+  
 end
