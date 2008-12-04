80f42a34511bd6002bf55059660f0b3721bdcc8a
Gordon McCreight <gordon@mccreight.com>
2008-12-03 16:39:57 -0800

    added a note about Jason Burnett

--- lib/creole.rb
+++ lib/creole.rb
@@ -1,5 +1,8 @@
 class Creole
 
+	# Most of this code is based off of Jason Burnett's excellent Perl-based converter which can be found here:
+	# http://search.cpan.org/~jburnett/Text-WikiCreole/
+
 	def self.strip_leading_and_trailing_eq_and_whitespace(s)
 	  s.sub!(/^\s*=*\s*/, '')
 	  s.sub!(/\s*=*\s*$/, '')
