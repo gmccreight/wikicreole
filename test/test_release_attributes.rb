4b871d781b1eb29f99b420448d4d27f52499a254
Gordon McCreight <gordon@mccreight.com>
2009-08-10 13:34:25 -0700

    rearranged the gemspec file and added an author test

---
+++ test/test_release_attributes.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'test/unit'
+
+class TC_WikiCreole < Test::Unit::TestCase
+
+  def setup
+
+    # Read the actual library code to get the version and the date
+    code = File.read("lib/wiki_creole.rb")
+    @version_from_code = code.match(/Version:: *(\d+\.\d+\.\d+)/)[1]
+    @date_from_code = code.match(/Date:: *(\d+-\d+-\d+)/)[1]
+
+    # Read the first line of the Changelog to get the version and the date
+    changelog = File.read("Changelog")
+    matches = changelog.match(/^ *(\d+-\d+-\d+) *\((\d+\.\d+\.\d+)\)/)
+    @date_from_changelog = matches[1]
+    @version_from_changelog = matches[2]
+    
+    # Read the gemspec file to get the version and the date
+    gemspec = File.read("wiki_creole_gemspec.rb")
+    @version_from_gemspec = gemspec.match(/s\.version.*?(\d+\.\d+\.\d+)/)[1]
+    @date_from_gemspec = gemspec.match(/s\.date.*?(\d+-\d+-\d+)/)[1]
+
+  end
+  
+  def test_versions_are_the_same_in_all_files
+    assert_equal @version_from_code, @version_from_changelog
+    assert_equal @version_from_changelog, @version_from_gemspec
+  end
+  
+  def test_dates_are_the_same_in_all_files
+    assert_equal @date_from_code, @date_from_changelog
+    assert_equal @date_from_changelog, @date_from_gemspec
+  end
+
+end
--- wiki_creole_gemspec.rb
+++ wiki_creole_gemspec.rb
@@ -1,50 +1,7 @@
+# -*- encoding: utf-8 -*-
+
 # You can create a new release using this file by calling:
 # gem build wiki_creole_gemspec.rb
-
-# Read the actual code to get the version and the date
-code = File.read("lib/wiki_creole.rb")
-version_from_code = code.match(/Version:: *(\d+\.\d+\.\d+)/m)[1]
-date_from_code = code.match(/Date:: *(\d+-\d+-\d+)/m)[1]
-
-# Read the first line of the Changelog to make sure it matches with the code's version and date.  This ensures
-# that everything is in sync for the release.
-changelog = File.read("Changelog")
-matches = changelog.match(/^ *(\d+-\d+-\d+) *\((\d+\.\d+\.\d+)\)/)
-date_from_changelog = matches[1]
-version_from_changelog = matches[2]
-
-had_errors = false
-
-if version_from_code != version_from_changelog
-  puts "  The version in the code --->#{version_from_code}<--- is not the
-  same as the version at the top of the Changelog --->#{version_from_changelog}<---"
-  had_errors = true
-end
-
-if date_from_code != date_from_changelog
-  puts "  The date in the code --->#{date_from_code}<--- is not the
-  same as the date at the top of the Changelog --->#{date_from_changelog}<---"
-  had_errors = true
-end
-
-if File.exists?("releases/WikiCreole-#{version_from_code}.gem")
-  puts "  It looks like the WikiCreole-#{version_from_code}.gem file was already released.
-  Look in the 'releases' folder to see it."
-  had_errors = true
-end
-
-if File.exists?("WikiCreole-#{version_from_code}.gem")
-  puts "  It looks like the WikiCreole-#{version_from_code}.gem file was already created, but maybe not released.
-  Look in the project's root folder.  If it was actually released, you should move it to the 'releases' folder.
-  Otherwise, if you just want to re-create the gem file, then delete the pre-existing one and run the command again."
-  had_errors = true
-end
-
-# If there were any errors, don't actually do the build.
-if had_errors
-  puts "BUILD ABORTED because of the errors listed above"
-  exit
-end
 
 files = %W{
   README
@@ -53,29 +10,45 @@ files = %W{
   LICENSE
   Rakefile
   lib/wiki_creole.rb
+}
+
+test_files = %W{
   test/profiling.rb
+  test/test_release_attributes.rb
   test/test_all.rb
-  test/test_amp.markup          test/test_amp.html
-  test/test_block.markup        test/test_block.html 
-  test/test_escape.markup       test/test_escape.html
-  test/test_inline.markup       test/test_inline.html
-  test/test_jsp_wiki.markup     test/test_jsp_wiki.html
-  test/test_nested_lists.markup test/test_nested_lists.html
-  test/test_specialchars.markup test/test_specialchars.html
+  test/test_amp.markup           test/test_amp.html
+  test/test_block.markup         test/test_block.html
+  test/test_escape.markup        test/test_escape.html
+  test/test_inline.markup        test/test_inline.html
+  test/test_jsp_wiki.markup      test/test_jsp_wiki.html
+  test/test_nested_lists.markup  test/test_nested_lists.html
+  test/test_specialchars.markup  test/test_specialchars.html
 }
 
 Gem::Specification.new do |s|
-   s.name = %q{WikiCreole}
-   s.rubyforge_project = 'wikicreole'
-   s.version = version_from_code
-   s.date = date_from_code
-   s.authors = ["Gordon McCreight"]
-   s.email = %q{gordon@mccreight.com}
-   s.has_rdoc = true
-   s.require_path = "lib"
-   s.summary = %q{A Creole-to-XHTML converter written in pure Ruby}
-   s.homepage = %q{http://github.com/gmccreight/wikicreole/tree/master}
-   s.description = %q{A Creole-to-XHTML converter written in pure Ruby}
-   s.files = files
-   s.test_files = "test/test_all.rb".to_a
+  s.name = %q{WikiCreole}
+  s.version = %q{0.1.3}
+  
+  s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version
+  
+  s.authors = ["Gordon McCreight"]
+  s.date = %q{2009-02-05}
+  s.description = %q{A Creole-to-XHTML converter written in pure Ruby}
+  s.email = %q{gordon@mccreight.com}
+  s.extra_rdoc_files = %W{README LICENSE}
+  s.files = files
+  s.has_rdoc = true
+  s.homepage = %q{http://github.com/gmccreight/wikicreole/}
+  s.rdoc_options = ["--inline-source", "--charset=UTF-8"]
+  s.require_paths = ["lib"]
+  s.rubyforge_project = %q{wikicreole}
+  s.rubygems_version = %q{1.3.0}
+  s.summary = %q{A Creole-to-XHTML converter written in pure Ruby}
+  s.test_files = test_files
+ 
+  if s.respond_to? :specification_version then
+    current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
+    s.specification_version = 2
+  end
+
 end
